91928c8d76ddf65a9e769868541eacd16bef16eb
Ernie Pedapati <ernest.pedapati@cchmc.org>
2023-03-11 03:50:00 -0500

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get install lsb --fix-missing
-wget https://ssd.mathworks.com/supportfiles/downloads/R2022b/license_manager/R2022b/daemons/glnxa64/mathworks_network_license_manager_glnxa64.zip
-unzip mathworks_network_license_manager_glnxa64.zip
-cd etc
-chmod +x lmutil.sh
-./lmutil lmstat -a
